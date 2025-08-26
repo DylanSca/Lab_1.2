e4f3dd4994b6aba3cdb13bbf84851f1b294c1f3a
Dylan <rockrz91919@gmail.com>
2025-08-26 15:22:13 -0400

    Did all the house questions 14-18

--- Picture.java
+++ Picture.java
@@ -53,11 +53,12 @@ public class Picture
             roof.moveVertical(-60);
             roof.makeVisible();
     
-            sun.changeColor("yellow");
+            sun.changeColor("blue");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            sun.slowMoveVertical(120);
             drawn = true;
             
             sun2.changeColor("red");
